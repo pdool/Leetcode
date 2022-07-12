c7cc8347c08cd35edc65e10156aeef14e1a76412
pdool <chong2008xin@qq.com>
2022-07-12 17:46:10 +0800

    tree

---
+++ src/main/java/com/xin/tree/easy/Sln530.java
@@ -0,0 +1,29 @@
+package com.xin.tree.easy;
+
+import common.TreeNode;
+
+public class Sln530 {
+    int pre;
+    int ans;
+
+    public int getMinimumDifference(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre == -1) {
+            pre = root.val;
+        } else {
+            ans = Math.min(ans, root.val - pre);
+            pre = root.val;
+        }
+        dfs(root.right);
+    }
+}
